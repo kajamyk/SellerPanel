dd875651cff53315417c7739ec1ec7e12677a34c
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-01-08 18:36:05 +0100

    Setup navigation from toolbar to main page

--- src/shared/Toolbar.js
+++ src/shared/Toolbar.js
@@ -1,6 +1,15 @@
+import {useNavigate} from "react-router-dom";
+
 export function Toolbar({title}) {
-    return <div className="toolbar">
-        <img className="toolbar-image" src={'drawable/money.svg'} alt="App logo"/>
-        <span style={{marginLeft: 24}} className="headline-large">{title}</span>
+    const navigate = useNavigate()
+
+    const commandNavigateToMain = (e) => {
+        e.preventDefault()
+        navigate("/")
+    }
+
+    return <div className="Toolbar">
+        <img onClick={commandNavigateToMain} src={'drawable/money.svg'} alt="App logo"/>
+        <span onClick={commandNavigateToMain} style={{marginLeft: 24}} className="headline-large">{title}</span>
     </div>
 }
