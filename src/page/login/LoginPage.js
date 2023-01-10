81a5ecfb524d2e5825273c2623bcd3f114fdc59a
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-01-10 22:37:19 +0100

    add login page

--- src/page/login/LoginPage.js
+++ src/page/login/LoginPage.js
@@ -1,5 +1,30 @@
+import {Card} from "../../shared/Card";
+import {useNavigate} from "react-router-dom";
+
 export function LoginPage() {
-    return <div>
-        Login
+    const navigate = useNavigate()
+    const commandLogin = (e) => {
+        e.preventDefault()
+        console.log("navigate to /")
+        navigate("/")
+    }
+
+    return <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
+        <Card>
+            <div style={{margin: 24}}>
+                <div style={{textAlign: "center", alignItems: "center"}} className="flex-column">
+                    <span className="display-medium">Logowanie</span>
+                    <img src={'drawable/login.svg'} alt="Login"/>
+                </div>
+                <form onSubmit={commandLogin} style={{marginTop: 24, marginLeft: 48, marginRight: 48}}
+                      className="flex-column">
+                    <span style={{marginBottom: 14}} className="headline-medium">Email</span>
+                    <input className="headline-medium" type="email" name="email" placeholder="email@example.com"/>
+                    <span style={{marginTop: 20, marginBottom: 14}} className="headline-medium">Hasło</span>
+                    <input className="headline-medium" type="password" name="password" placeholder="******"/>
+                    <button style={{marginTop: 32}} class="button-56" role="button">Zaloguj</button>
+                </form>
+            </div>
+        </Card>
     </div>
 }
