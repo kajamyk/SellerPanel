dbba6da639407a85badcd7208ceb217ffdc09b51
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-01-10 18:59:50 +0100

    Add color changes to OrdersPage

--- src/page/orders/OrdersPage.js
+++ src/page/orders/OrdersPage.js
@@ -2,8 +2,8 @@ import {Card} from "../../shared/Card";
 import {VerticalDivider} from "../../shared/VerticalDivider";
 import {Toolbar} from "../../shared/Toolbar";
 
-function OrderInfo({style, headline, amount, moneyValue}) {
-    return <div style={{...style, textAlign: "center", display: "flex", flexDirection: "column"}}>
+function OrderInfo({headline, amount, moneyValue}) {
+    return <div className="order-info">
         <span className="display-medium">{headline}</span>
         <span style={{marginTop: 16}} className="headline-large">{amount}</span>
         <span className="title-large">Kwota:</span>
@@ -12,20 +12,16 @@ function OrderInfo({style, headline, amount, moneyValue}) {
 }
 
 export function OrdersPage() {
-    const orderInfoStyle = {
-        flex: 1,
-        padding: 24
-    }
     return <div>
         <Toolbar title={"Zamówienia"}/>
         <div style={{margin: 24}}>
             <Card>
                 <div style={{height: "75vh"}} className="flex-row">
-                    <OrderInfo style={orderInfoStyle} headline={"Nieopłacone"} amount={10} moneyValue={100}/>
+                    <OrderInfo headline={"Nieopłacone"} amount={10} moneyValue={100}/>
                     <VerticalDivider/>
-                    <OrderInfo style={orderInfoStyle} headline={"Niewysłane"} amount={10} moneyValue={100}/>
+                    <OrderInfo headline={"Niewysłane"} amount={10} moneyValue={100}/>
                     <VerticalDivider/>
-                    <OrderInfo style={orderInfoStyle} headline={"Zwroty"} amount={10} moneyValue={100}/>
+                    <OrderInfo headline={"Zwroty"} amount={10} moneyValue={100}/>
                 </div>
             </Card>
         </div>
